94f71d2ac138ed889c6283abe9d324bec5b3fc5b
Keith Ko <ko.keith12@gmail.com>
2021-01-26 17:40:27 -0500

    find member and constituency from report xml

--- scripts/5_populate_expenditures_table.rb
+++ scripts/5_populate_expenditures_table.rb
@@ -11,11 +11,19 @@ previous_years_div.each do |report_div|
   reports_button_divs.each do |quarter_report_div|
     html_quarter = URI.parse("https://www.ourcommons.ca#{quarter_report_div.children[1].attribute('href').text}").open
     doc_quarter = Nokogiri::HTML(html_quarter)
-    summary_link = "https://www.ourcommons.ca/PublicDisclosure/#{doc_quarter.search('a#ctl00_lnkSummaryExpenditures\\.aspx')[0].attribute('href').text}&FormatType=XML"
     expend_by_members_link = "https://www.ourcommons.ca/PublicDisclosure/#{doc_quarter.search('a#ctl00_lnkMemberExpenditures\\.aspx')[0].attribute('href').text}&FormatType=XML"
-    summary_xml = Nokogiri::XML(URI.parse(summary_link).open).root
     expend_by_members_xml = Nokogiri::XML(URI.parse(expend_by_members_link).open).root
 
-    binding.pry
+    start_date = expend_by_members_xml.attribute('startDate').text
+    end_date = expend_by_members_xml.attribute('endDate').text
+
+    expend_by_members_xml.css('Report').each do |report|
+      constit = Constituency.find_by(name: report.css('Constituency').first.attribute('name-en').text.gsub('—', '-'))
+      first_name = report.css('Member').first.attribute('firstName').text.downcase
+      last_name = report.css('Member').first.attribute('lastName').text.downcase
+      member = Member.find_by(first_name: first_name, last_name: last_name)
+
+      binding.pry
+    end
   end
 end

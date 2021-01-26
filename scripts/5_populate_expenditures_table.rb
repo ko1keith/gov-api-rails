73af85057bd3fa17ad6bc730b884c0469b4d66f2
Keith Ko <ko.keith12@gmail.com>
2021-01-26 15:32:32 -0500

    obtain expenditure reports xml

--- scripts/5_populate_expenditures_table.rb
+++ scripts/5_populate_expenditures_table.rb
@@ -1,9 +1,21 @@
 require 'open-uri'
 puts 'Starting Script...'
 base_url = 'https://www.ourcommons.ca/ProactiveDisclosure/en/members/previous-reports'
-html = URI.open(base_url)
+html = URI.parse(base_url).open
 doc = Nokogiri::HTML(html)
+previous_years_div = doc.css('.reports-previous-specific-year-container')
+previous_years_div.each do |report_div|
+  year = report_div.attribute('data-year').text
+  reports_button_divs = report_div.css('.reports-previous-buttons-4')
 
-doc.css('.reports-previous-specific-year-container').each do |report_div|
-  binding.pry
+  reports_button_divs.each do |quarter_report_div|
+    html_quarter = URI.open("https://www.ourcommons.ca#{quarter_report_div.children[1].attribute('href').text}")
+    doc_quarter = Nokogiri::HTML(html_quarter)
+    summary_link = "https://www.ourcommons.ca/PublicDisclosure/#{doc_quarter.search('a#ctl00_lnkSummaryExpenditures\\.aspx')[0].attribute('href').text}&FormatType=XML"
+    expend_by_members_link = "https://www.ourcommons.ca/PublicDisclosure/#{doc_quarter.search('a#ctl00_lnkMemberExpenditures\\.aspx')[0].attribute('href').text}&FormatType=XML"
+    summary_xml = Nokogiri::XML(URI.open(summary_link)).root
+    expend_by_members_xml = Nokogiri::XML(URI.open(expend_by_members_link)).root
+
+    binding.pry
+  end
 end

54d6c9f9164f01886b81fc49c2c43733b46da512
Keith Ko <ko.keith12@gmail.com>
2021-02-03 13:15:19 -0500

    add expenditures requests specs

--- spec/requests/api/v1/expenditure_request_spec.rb
+++ spec/requests/api/v1/expenditure_request_spec.rb
@@ -1,4 +1,4 @@
-require 'rails-helper'
+require 'rails_helper'
 
 RSpec.describe 'Expenditures', type: :request do
   describe 'GET /index' do
@@ -7,5 +7,15 @@ RSpec.describe 'Expenditures', type: :request do
       get '/api/v1/expenditures', headers: headers
       expect(response).to have_http_status(:success)
     end
+  end
+
+  it 'JSON body response contains expected expenditure attributes' do
+    headers = { 'ACCEPT' => 'application/json' }
+    get '/api/v1/expenditures', headers: headers
+    json_response = JSON.parse(response.body)
+
+    expect(response).to have_http_status(:success)
+    expect(json_response['data'][0]['attributes'].keys).to match_array(%w[category subcategory start_date
+                                                                          end_date member_budget resources_provided_by_house total party member])
   end
 end
